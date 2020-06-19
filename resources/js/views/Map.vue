2ad61934df864f2b2b45b1745d9f5d7756af2c4a
Tim <34155548+TotallyTheTim@users.noreply.github.com>
2020-06-19 16:26:43 +0200

    make modal

--- resources/js/views/Map.vue
+++ resources/js/views/Map.vue
@@ -1,87 +1,101 @@
 <template>
     <div>
-        <div ref="mapHolder" class="map-holder" />
+        <div ref="mapHolder" class="map-holder"/>
     </div>
 </template>
 
 <script>
-import API from '../Api';
-import interact from 'interactjs';
-import create from 'dom-create-element';
+    import API from '../Api';
+    import interact from 'interactjs';
+    import create from 'dom-create-element';
 
-export default {
-    name: 'Map',
-    data () {
-        return {
-            event_id: this.$route.params.event_id
-        };
-    },
-    methods: {
-        init () {
-            interact('.draggable')
-                .on('tap', function (event) {
-                    window.dispatchEvent(new CustomEvent('show-modal', { detail: event }));
-                });
-            window.addEventListener('show-modal', this.showModal, false);
+    export default {
+        name: 'Map',
+        data() {
+            return {
+                allowedIn: true,
+                event_id: this.$route.params.event_id,
+                mapData: null,
+            };
         },
-        createNewDomElement (counter, backgroundColorCodeItem, width, height, parentOffsetX, parentOffsetY) {
-            const item = create({
-                selector: 'div',
-                id: `stand-id-${counter}`,
-                styles: 'draggable',
-                children: create({
-                    selector: 'p',
-                    html: `stand-id-${counter}`
-                })
-            });
+        methods: {
+            init() {
+                let _this = this;
+                interact('.draggable')
+                    .on('tap', function (e) {
+                        _this.showModal(e.currentTarget.id)
+                    });
+                window.addEventListener('show-modal', this.showModal, false);
+            },
+            createNewDomElement(counter, backgroundColorCodeItem, width, height, parentOffsetX, parentOffsetY) {
+                const item = create({
+                    selector: 'div',
+                    id: `stand-id-${counter}`,
+                    styles: 'draggable',
+                    children: create({
+                        selector: 'p',
+                        html: `stand-id-${counter}`
+                    })
+                });
 
-            item.style.backgroundColor = backgroundColorCodeItem;
-            item.style.width = width + 'px';
-            item.style.height = height + 'px';
+                item.style.backgroundColor = backgroundColorCodeItem;
+                item.style.width = width + 'px';
+                item.style.height = height + 'px';
 
-            item.style.webkitTransform =
+                item.style.webkitTransform =
                     item.style.transform =
                         'translate(' + parentOffsetX + 'px, ' + parentOffsetY + 'px)';
-            item.setAttribute('data-x', parentOffsetX);
-            item.setAttribute('data-y', parentOffsetY);
+                item.setAttribute('data-x', parentOffsetX);
+                item.setAttribute('data-y', parentOffsetY);
+
+                const container = this.$refs.mapHolder;
+                container.appendChild(item);
+            },
+            showModal(id) {
+                if (!this.allowedIn){
+                    return;
+                }
+                let link  = this.mapData.items.filter(i => i.id === id)[0].url;
+                link = JSON.parse(JSON.stringify(link));
+
+                //TODO: make modal?
+            }
+        },
+        async mounted() {
+            this.showModal = this.showModal.bind(this);
+            const res = await API.get('/api/event/map/' + this.event_id);
+            const data = res.data;
+
+
+            const mapData = JSON.parse(data.json);
+            this.mapData = mapData;
+
 
             const container = this.$refs.mapHolder;
-            container.appendChild(item);
+            container.style.minWidth = mapData.map.width + 'px';
+            container.style.minHeight = mapData.map.height + 'px';
+
+            let counter = 1;
+            mapData.items.forEach((el) => {
+                this.createNewDomElement(
+                    counter,
+                    el.style.backgroundColor,
+                    el.style.width,
+                    el.style.height,
+                    el.positionFromParent.x,
+                    el.positionFromParent.y
+                );
+                counter++;
+            });
+
+            const resp = await API.get('/api/map/register/');
+            if (!!resp.data) {
+                this.allowedIn = true;
+            }
+            this.init();
         },
-        showModal (event) {
-            console.log(event);
+        beforeDestroy() {
+            window.removeEventListener('show-modal', this.showModal, false);
         }
-    },
-    async mounted () {
-        this.showModal = this.showModal.bind(this);
-        const res = await API.get('/api/event/map/' + this.event_id);
-        const data = res.data;
-
-        const mapData = JSON.parse(data.json);
-
-        const container = this.$refs.mapHolder;
-        container.style.minWidth = mapData.map.width + 'px';
-        container.style.minHeight = mapData.map.height + 'px';
-
-        let counter = 1;
-        mapData.items.forEach((el) => {
-            this.createNewDomElement(
-                counter,
-                el.style.backgroundColor,
-                el.style.width,
-                el.style.height,
-                el.positionFromParent.x,
-                el.positionFromParent.y
-            );
-            counter++;
-        });
-
-        const permissions = await API.get('/api/map/register/');
-        console.log(permissions);
-        this.init();
-    },
-    beforeDestroy () {
-        window.removeEventListener('show-modal', this.showModal, false);
-    }
-};
+    };
 </script>
