512d31ecf83830a2cc1df4cc51490060d4e9020a
Merlijn Busch <24628@ma-web.nl>
2020-06-19 18:33:39 +0200

    small update

--- resources/js/views/QRCode.vue
+++ resources/js/views/QRCode.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="qr-container">
-            qr code generator
+        <p class="qr-title">Uw ticket laat dit scannen bij ingang evenement</p>
         <qrcode-vue :value="value" :size="size" level="H"></qrcode-vue>
     </div>
 </template>
 
 <script>
-    import QrcodeVue from 'qrcode.vue'
+    import QrcodeVue from 'qrcode.vue';
 
     export default {
         name: 'QRCode',
@@ -20,9 +20,7 @@
                 size: 300,
             };
         },
-        methods: {
-
-        },
+        methods: {},
         mounted () {
 
         }
@@ -36,5 +34,11 @@
         flex-direction: column;
         align-items: center;
         margin-top: 100px;
+    }
+
+    .qr-title {
+        color: whitesmoke;
+        font-size: large;
+        margin: 20px 0;
     }
 </style>
