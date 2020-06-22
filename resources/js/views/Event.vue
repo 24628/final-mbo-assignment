54350503a82179d009b7297b73dfb04c7db89314
Tim <34155548+TotallyTheTim@users.noreply.github.com>
2020-06-22 16:02:49 +0200

    time date fix mac

--- resources/js/views/Event.vue
+++ resources/js/views/Event.vue
@@ -566,16 +566,9 @@ export default {
             }
         },
         getTime (dateObj) {
-            const date = new Date(dateObj);
-            let hours = date.getHours().toString();
-            let minutes = date.getMinutes().toString();
-            if (hours.length === 1) {
-                hours = '0' + hours;
-            }
-            if (minutes.length === 1) {
-                minutes = '0' + minutes;
-            }
-            return hours + ':' + minutes;
+            let arr = dateObj.split(" ");
+            let out = arr[arr.length-1];
+            return out;
         },
         setSpeaker (index, speaker, time, isKeynote = false) {
             speaker = JSON.parse(JSON.stringify(speaker));
