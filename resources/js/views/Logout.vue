8026154f4fa5041c20354dc46fffda626acb8f56
Tim <34155548+TotallyTheTim@users.noreply.github.com>
2020-06-22 16:41:14 +0200

    logout validation fix

---
+++ resources/js/views/Logout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Logging out.
+    </div>
+</template>
+
+<script>
+export default ({
+    mounted () {
+        this.$emit('logout');
+        this.$router.replace("/");
+    }
+});
+</script>
